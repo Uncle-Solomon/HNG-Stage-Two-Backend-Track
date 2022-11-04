db8d16976b3c03c560db2cf30202f000ec5011a7
Uncle-Solomon <106064395+Uncle-Solomon@users.noreply.github.com>
2022-11-04 13:28:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
-
+@app.route('/')
+def something():
+    return "vibes"
 @app.route('/api/v1', methods=['POST'])
 def calculatorApi():
     body = request.get_json()
