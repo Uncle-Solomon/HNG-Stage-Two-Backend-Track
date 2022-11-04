b1c98b3111c0e8e6f99a9943cd34372ebdb079f6
Uncle-Solomon <106064395+Uncle-Solomon@users.noreply.github.com>
2022-11-04 13:42:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,56 +4,43 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods = ['GET'])
-def anApi():
-	if(request.method == 'GET'):
-		data = {
-			"slackUsername": "Uncle-Solomon",
-            "backend": True,
-            "age" : 20,
-            "bio": "A Nigerian Prince trying to find his way in the tech industry."
-		}
-		return jsonify(data)
-# @app.route('/', methods=['GET'])
-# def something():
-#     return "vibes"
-# @app.route('/api/v1', methods=['POST'])
-# def calculatorApi():
-#     body = request.get_json()
+@app.route('/', methods=['POST'])
+def calculatorApi():
+    body = request.get_json()
 
-#     operation_types = body['operation_type']
-#     op_types = operation_types.split()
-#     for op in op_types:
-#         if op == "addition" or op == "add" or op == "plus" or op == "sum":
-#             operation_type = "addition"
-#         elif op == "subtraction" or op == "minus" or op == "subtract" or op == "difference":
-#             operation_type = "subtraction"
-#         elif op == "multiplication" or op == "times" or op == "multiply" or op == "product":
-#             operation_type = "multiplication"
-#     x = body['x']
-#     y = body['y']
+    operation_types = body['operation_type']
+    op_types = operation_types.split()
+    for op in op_types:
+        if op == "addition" or op == "add" or op == "plus" or op == "sum":
+            operation_type = "addition"
+        elif op == "subtraction" or op == "minus" or op == "subtract" or op == "difference":
+            operation_type = "subtraction"
+        elif op == "multiplication" or op == "times" or op == "multiply" or op == "product":
+            operation_type = "multiplication"
+    x = body['x']
+    y = body['y']
 
-#     try:
-#         if operation_type != None and x != None and y != None:
-#             operation_type = operation_type.lower().strip()
-#             if (operation_type == "addition" or operation_type == "subtraction" or operation_type == "multiplication"):
-#                 if operation_type == "addition":
-#                     result = x + y
-#                 elif operation_type == "subtraction":
-#                     result = x - y
-#                 elif operation_type == "multiplication":
-#                     result = x * y
-#             else:
-#                 abort(400)
-#         else:
-#             abort(400)
-#     except Exception as e:
-#         return e
+    try:
+        if operation_type != None and x != None and y != None:
+            operation_type = operation_type.lower().strip()
+            if (operation_type == "addition" or operation_type == "subtraction" or operation_type == "multiplication"):
+                if operation_type == "addition":
+                    result = x + y
+                elif operation_type == "subtraction":
+                    result = x - y
+                elif operation_type == "multiplication":
+                    result = x * y
+            else:
+                abort(400)
+        else:
+            abort(400)
+    except Exception as e:
+        return e
     
-#     return jsonify({
-#             'slackUsername': 'Uncle-Solomon',
-#             'result': result,
-#         })
+    return jsonify({
+            'slackUsername': 'Uncle-Solomon',
+            'result': result,
+        })
 
 if __name__=='__main__':
     app.run(debug=True)
