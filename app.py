8ea6f58f801287f1af7545b6c35167ffe2c61a8a
Uncle-Solomon <106064395+Uncle-Solomon@users.noreply.github.com>
2022-11-04 13:30:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
-@app.route('/')
+@app.route('/', methods=['GET'])
 def something():
     return "vibes"
 @app.route('/api/v1', methods=['POST'])
